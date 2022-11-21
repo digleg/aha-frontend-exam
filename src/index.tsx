9c702084508b6c7b5c4e2c0bc0f0be0f7a9addee
digleg <digleg_h@hotmail.com>
2022-11-21 12:31:23 +0800

    feat: ubuntu family settings

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
+import './fonts/Ubuntu/Ubuntu-Bold.ttf';
+import './fonts/Ubuntu/Ubuntu-Light.ttf';
+import './fonts/Ubuntu/Ubuntu-Medium.ttf';
+import './fonts/Ubuntu/Ubuntu-Regular.ttf';
+import './fonts/Open_Sans/OpenSans-VariableFont_wdth.ttf';
+
 import { store } from './redux/store';
 import App from './App';
 
